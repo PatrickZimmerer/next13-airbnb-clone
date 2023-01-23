cb316da86bcecc42708f49f5e766b450d1c55687
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-01-23 22:58:55 +0100

    finished basic layout for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
 import logo from '../images/airbnb-logo.png';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import {
+	MagnifyingGlassIcon,
+	GlobeAltIcon,
+	UserCircleIcon,
+	Bars3Icon,
+} from '@heroicons/react/24/solid';
 
 function Header() {
 	return (
@@ -11,13 +16,20 @@ function Header() {
 			</div>
 			<div className="flex items-center rounded-full px-2 md:border-2 md:shadow-sm">
 				<input
-					className="outline-none flex-grow pl-2 bg-transparent"
+					className="outline-none flex-grow pl-2 bg-transparent text-gray-600 placeholder-gray-400"
 					type="text"
 					placeholder="Start your Search"
 				/>
 				<MagnifyingGlassIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex" />
 			</div>
-			<div></div>
+			<div className="flex items-center space-x-4 justify-end text-gray-500">
+				<p className="hidden md:inline-flex cursor-pointer">Become a host</p>
+				<GlobeAltIcon className="h-6 cursor-pointer" />
+				<div className="flex items-center p-2 rounded-full border-2 space-x-2 ">
+					<Bars3Icon className="h-6 cursor-pointer" />
+					<UserCircleIcon className="h-6 cursor-pointer" />
+				</div>
+			</div>
 		</header>
 	);
 }
