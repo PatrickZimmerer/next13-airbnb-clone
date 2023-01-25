0d3c0fb88eee69285029f1255814eda0ae715282
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-01-25 20:39:41 +0100

    improved largeCard responsive design

--- components/LargeCard.tsx
+++ components/LargeCard.tsx
@@ -14,7 +14,7 @@ function LargeCard({ img, title, description, buttonText }: Props) {
 			<div className="relative sm:h-96 h-64">
 				<Image src={img} fill alt="location-background-image" className="object-cover rounded-xl" />
 			</div>
-			<div className="absolute top-32 left-12">
+			<div className="absolute top-1/4 left-12">
 				<h2 className="pb-2 text-lg sm:text-4xl">{title}</h2>
 				<p className="pb-4">{description}</p>
 				<button className="text-white bg-black shadow-sm hover:shadow-lg active:scale-95 transition duration-200 rounded-xl px-4 py-2">
